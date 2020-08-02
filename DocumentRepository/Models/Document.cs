f9338627a088889d6d7fc22b01bafd065d7f52fc
Michael Shannon <mgshannon1s@gmail.com>
2020-08-02 15:45:18 -0500

    Adding data model and entity framework to store the model in entity db context

---
+++ DocumentRepository/Models/Document.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DocumentRepository.Models
+{
+    public class Document
+    {
+        public long Id;
+        public string Name;
+        public byte[] Contents;
+    }
+}
---
+++ DocumentRepository/Models/DocumentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentRepository.Models
+{
+    public class DocumentContext : DbContext
+    {
+
+        public DocumentContext(DbContextOptions<DocumentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Document> TodoItems { get; set; }
+
+    }
+}
